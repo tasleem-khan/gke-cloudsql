de94023c73a45cdc1d4d16708508e52863ff9210
tasleem.khan@66degrees.com <tasleem.khan@66degrees.com>
2023-10-29 21:58:01 +0530

    Modifying output

--- envs/development/outputs.tf
+++ envs/development/outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_link" {
   description = "VPC LINK"
-  value = module.vpc.network_self_link
+  value = module.env.vpc_link
 }
 
 output "cloud-sql" {
   description = "CLOUD SQL"  
-  value =  resource.google_sql_database_instance.main_primary.self_link
+  value =  module.env.cloud-sql
 }
