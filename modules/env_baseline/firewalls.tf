a71d47b6ed8d2ffb20c91e7cf9f8a2eaee36e618
tasleem.khan@66degrees.com <tasleem.khan@66degrees.com>
2023-11-01 00:07:15 +0530

    Modifying Firewall

--- modules/env_baseline/firewalls.tf
+++ modules/env_baseline/firewalls.tf
@@ -1,13 +1,13 @@
-resource "google_compute_firewall" "iap_access" {
-  allow {
-    protocol =  "tcp"
-    ports     = ["9080","9081"]
-  }
-  description   = "IAP ACCESS"
-  direction     = "INGRESS"
-  name          = "custom-allow-iap"
-  network       = module.vpc.network_name
-  priority      = 1000
-  project       = module.host-project.project_id
-  source_ranges = ["130.211.0.0/22","35.191.0.0/16"]
-}
+# resource "google_compute_firewall" "iap_access" {
+#   allow {
+#     protocol =  "tcp"
+#     ports     = ["9080","9081"]
+#   }
+#   description   = "IAP ACCESS"
+#   direction     = "INGRESS"
+#   name          = "custom-allow-iap"
+#   network       = module.vpc.network_name
+#   priority      = 1000
+#   project       = module.host-project.project_id
+#   source_ranges = ["130.211.0.0/22","35.191.0.0/16"]
+# }
